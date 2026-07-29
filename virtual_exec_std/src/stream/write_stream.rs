00e2ded2ab987ae59a3ecfd583a2e7c28433ad22
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2026-07-29 10:44:00 +0100

    Add instruction cost by amount of stream writing

--- virtual_exec_std/src/stream/write_stream.rs
+++ virtual_exec_std/src/stream/write_stream.rs
@@ -4,20 +4,28 @@ use async_lock::RwLock;
 use cfg_if::cfg_if;
 use tokio::io;
 use tokio::io::AsyncWriteExt;
+use virtual_exec_core::fn_extern::fn_args::FnExternArg::Machine;
 use crate::stream::{OutputByteStream, OutputByteStreamInner};
 use virtual_exec_extern::*;
 use virtual_exec_type::base::Native;
-use virtual_exec_type::ext::SafeReadArcExt;
+use virtual_exec_type::error::{ExecutionError, RecoverableError};
+use virtual_exec_type::ext::{SafeLockArcExt, SafeReadArcExt};
 use virtual_exec_type::vm_type::{Boolean, Error};
 
 #[fn_extern_wrap]
-fn write_stream_sync(Native(stream): Native<OutputByteStream>, data: String) -> Result<Boolean, Error> {
+fn write_stream_sync<'__wrap_internal2, 'a>(Native(stream): Native<OutputByteStream>, data: String, Machine(machine): _) -> Result<Boolean, Error> {
+    if machine.lock_arc_safe().machine.lim.saturating_sub(data.len() as u64) == 0 {
+        return Err(ExecutionError::Recoverable(RecoverableError::TimeoutError(data.len() as u64)))
+    }
     let data: Vec<u8> = data.into_bytes();
     Ok(stream.read_arc_safe().sync_fn.f.deref()(data))
 }
 
 #[fn_extern_wrap_async]
-async fn write_stream_async(Native(stream): Native<OutputByteStream>, data: String) -> Result<Boolean, Error> {
+async fn write_stream_async<'a, '__wrap_internal2>(Native(stream): Native<OutputByteStream>, data: String, Machine(machine): _) -> Result<Boolean, Error> {
+    if machine.lock_arc_safe().machine.lim.saturating_sub(data.len() as u64) == 0 {
+        return Err(ExecutionError::Recoverable(RecoverableError::TimeoutError(data.len() as u64)))
+    }
     let data: Vec<u8> = data.into_bytes();
     let stream = stream.read().await;
     if let Some(f) = &stream.async_fn {
