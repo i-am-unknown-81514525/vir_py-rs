05f1909cb647fcb4e9f6ee2fc0673c14cb018d33
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2026-08-06 18:38:29 +0100

    Add get_owned from allocator wrapper

--- virtual_exec_wasm/src/types/alloc.rs
+++ virtual_exec_wasm/src/types/alloc.rs
@@ -1,6 +1,11 @@
+use wasm_bindgen::JsValue;
 use wasm_bindgen::prelude::wasm_bindgen;
 use virtual_exec_core::Machine;
-use virtual_exec_type::mem::MemoryAllocator;
+use virtual_exec_type::ext::SafeLockArcExt;
+use virtual_exec_type::mem::{MemoryAllocator, OwnedValue};
+use crate::error::Error;
+use crate::types::owned::OwnedValueWrapper;
+use crate::types::ValuePtrWrapper;
 
 #[wasm_bindgen]
 pub struct AllocatorWrapper(MemoryAllocator<'static>);
@@ -14,5 +19,15 @@ impl AllocatorWrapper {
 impl From<MemoryAllocator<'static>> for AllocatorWrapper {
     fn from(value: MemoryAllocator<'static>) -> Self {
         Self(value)
+    }
+}
+
+#[wasm_bindgen]
+impl AllocatorWrapper {
+    #[wasm_bindgen]
+    pub fn get_owned(&mut self, value: ValuePtrWrapper) -> Result<OwnedValueWrapper, JsValue> {
+        self.0.lock_arc_safe().get_owned(&value.0)
+            .map_err(|e| e.to_js_error("Memory out of bound"))
+            .map(|v| v.into())
     }
 }
