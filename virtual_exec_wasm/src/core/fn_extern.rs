bfb8dc5f416f8b8c1a19950bccdfd4459e1b9af0
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2026-08-06 11:32:14 +0100

    Add JsValue to ValuePtr reconstruction

--- virtual_exec_wasm/src/core/fn_extern.rs
+++ virtual_exec_wasm/src/core/fn_extern.rs
@@ -1,17 +1,41 @@
 use std::collections::{HashSet, VecDeque};
+use std::fmt::{Debug, Formatter};
+use std::sync::Arc;
+use async_lock::RwLock;
 use wasm_bindgen::JsValue;
 use wasm_bindgen::prelude::wasm_bindgen;
 use virtual_exec_core::fn_extern::{FnExtern, FnExternConstruct};
 use virtual_exec_core::Machine;
-use virtual_exec_type::error::{CriticalError, ExecutionError, NonRecoverableError};
+use virtual_exec_type::base::{TypeCast, VmAnyType};
+use virtual_exec_type::error::{CriticalError, ExecutionError, MemoryError, NonRecoverableError};
+use virtual_exec_type::ext::SafeWriteArcExt;
 use virtual_exec_type::HashMap;
-use virtual_exec_type::mem::{ValueInnerPtr, ValuePtr};
+use virtual_exec_type::mem::{Allocator, MemoryAllocator, Value, ValueInnerPtr, ValuePtr};
 use crate::core::{lifetime_transmute_machine_ref_mut};
 use crate::core::machine_ref::MachineRef;
 use crate::types::{extend_ptr, ValuePtrWrapper};
 
 #[wasm_bindgen]
 pub struct JsExternFuncSync(Option<js_sys::Function>);
+
+#[wasm_bindgen]
+#[derive(Debug, Clone)]
+pub struct JsValueWrapper(#[wasm_bindgen(getter_with_clone)] pub JsValue);
+
+impl From<JsValue> for JsValueWrapper {
+    fn from(v: JsValue) -> Self {
+        Self(v)
+    }
+}
+
+
+impl VmAnyType for JsValueWrapper {
+    fn get_size(&self) -> usize {
+        1024
+    }
+}
+
+
 
 impl FnExternConstruct for JsExternFuncSync {
     fn new() -> Self
@@ -77,12 +101,12 @@ fn get_id(seen: &mut Vec<JsValue>, v: &JsValue) -> usize {
     seen.len() - 1
 }
 
-pub(crate) fn get_id_link(value: &JsValue) -> (HashMap<usize, GraphLink>, HashMap<usize, JsValue>) {
-    let mut map1: HashMap<usize, GraphLink> = HashMap::new();
-    let mut map2: HashMap<usize, JsValue> = HashMap::new();
+pub(crate) fn get_id_link(value: &JsValue) -> (usize, HashMap<usize, (JsValue, GraphLink)>) {
+    let mut map2: HashMap<usize, (JsValue, GraphLink)> = HashMap::new();
     let mut pending: VecDeque<JsValue> = VecDeque::new();
     let mut seen: Vec<JsValue> = Vec::new();
     pending.push_back(value.clone());
+    let id = get_id(&mut seen, &value);
     while let Some(item) = pending.pop_front() {
         let item_id = get_id(&mut seen, &item);
         let mut discovered: Vec<(JsValue, usize)> = Vec::new();
@@ -109,13 +133,65 @@ pub(crate) fn get_id_link(value: &JsValue) -> (HashMap<usize, GraphLink>, HashMa
             if pending.contains(&item) {
                 return;
             }
-            if map1.contains_key(&idx) {
+            if map2.contains_key(&idx) {
                 return;
             }
             pending.push_back(item);
         });
-        map2.insert(item_id, item);
-        map1.insert(item_id, link);
+        map2.insert(item_id, (item, link));
     }
-    (map1, map2)
+    (id, map2)
+}
+fn to_value_uninit(js: &JsValue) -> Value<'static> {
+    if let Some(v) = js.as_f64() {
+        Value::Float(v)
+    } else if let Some(v) = js.as_string() {
+        Value::String(v.into_boxed_str())
+    } else if let Some(v) = js.as_bool() {
+        Value::Bool(v)
+    } else if js.is_null_or_undefined() {
+        Value::None
+    } else if js.is_array() {
+        Value::Collection(Arc::new(RwLock::new(Vec::new())))
+    } else if js.is_object() {
+        Value::Object(Arc::new(RwLock::new(HashMap::new())))
+    } else {
+        Value::Any(Arc::new(RwLock::new(JsValueWrapper::from(js.clone()))))
+    }
+}
+
+fn clone_construct(value: &JsValue, alloc: MemoryAllocator<'static>) -> Result<ValuePtr<'static>, ExecutionError> {
+    let (id, link_map) = get_id_link(&value);
+    let mut new_ref: HashMap<usize, (ValuePtr<'static>, GraphLink)> = link_map.into_iter()
+        .map(|(k, v)| Ok((k, (alloc.alloc(to_value_uninit(&v.0))?, v.1))))
+        .collect::<Result<HashMap<usize, (ValuePtr<'static>, GraphLink)>, MemoryError>>()?;
+    let immutable: HashMap<usize, ValuePtr<'static>> = new_ref.iter().map(|(k, v)| (k.clone(), v.0.clone())).collect();
+    for (k, (v, link)) in new_ref.iter_mut() {
+        match link {
+            GraphLink::Object(map) => {
+                if let Some(inner) = v.as_object() {
+                    let mut lock = inner.write_arc_safe();
+                    for (k, v) in map.iter() {
+                        let ptr = immutable.get(v).expect("Value in new_ref should exist in immutable");
+                        lock.insert(k.clone(), ptr.clone());
+                    }
+                } else {
+                    unreachable!("Should be object on creation")
+                }
+            },
+            GraphLink::Collection(vec) => {
+                if let Some(inner) = v.as_collections() {
+                    let mut lock = inner.write_arc_safe();
+                    for v in vec.iter() {
+                        let ptr = immutable.get(v).expect("Value in new_ref should exist in immutable");
+                        lock.push(ptr.clone());
+                    }
+                } else {
+                    unreachable!("Should be collection on creation")
+                }
+            },
+            GraphLink::None => {}
+        }
+    };
+    Ok(new_ref.remove(&id).unwrap().0)
 }
