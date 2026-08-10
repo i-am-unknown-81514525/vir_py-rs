9b5b1ec656acb9b7611af14ff9078b19025f0bb5
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2026-08-10 19:26:00 +0100

    Add stop button

--- demo_site/script.js
+++ demo_site/script.js
@@ -23,22 +23,36 @@ const INSTRUCTION_LIMIT = 200_000n;
 
 let is_running = false;
 
+function set_running_state(state) {
+    if (state) {
+        document.getElementById("run_button").classList.add("hidden");
+        document.getElementById("stop_button").classList.remove("hidden");
+        is_running = true;
+    } else {
+        document.getElementById("run_button").classList.remove("hidden");
+        document.getElementById("stop_button").classList.add("hidden");
+        is_running = false;
+    }
+}
+
 async function run() {
     clearOutput();
-    is_running = true;
+    set_running_state(true);
     const machine = new MachineWrapper(MEMORY_LIMIT, INSTRUCTION_LIMIT);
     try {
         machine.push_code(editor.getValue());
         while (is_running) {
             let r = machine.sync_run_for(2000n);
             if (!r.can_continue_executing) {
-                is_running = false;
+                set_running_state(false);
             }
         }
     } finally {
         machine.free();
     }
-    is_running = false;
+    set_running_state(false);
 }
 
 document.getElementById("run_button").addEventListener("click", run);
+document.getElementById("stop_button").addEventListener("click", () => set_running_state(false));
+
