222a25ad57343643b290707866b65bf597539fbb
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2026-08-10 19:44:06 +0100

    Add standard library functions and override get_output_stream

--- demo_site/script.js
+++ demo_site/script.js
@@ -1,4 +1,4 @@
-import init, { MachineWrapper } from "./pkg/virtual_exec_js.js";
+import init, {Builtin, FnStreamOutput, MachineWrapper} from "./pkg/virtual_exec_js.js";
 
 await init();
 
@@ -35,17 +35,26 @@ function set_running_state(state) {
     }
 }
 
+const streamDecoder = new TextDecoder("utf-8");
+function vm_writer(arr) {
+    write(streamDecoder.decode(arr))
+}
+
 async function run() {
     clearOutput();
     set_running_state(true);
     const machine = new MachineWrapper(MEMORY_LIMIT, INSTRUCTION_LIMIT);
+    let output = new FnStreamOutput(vm_writer);
+    machine.push_resolver(new Builtin().default())
+    machine.push_resolver(output.to_resolver("get_output_stream"));
     try {
         machine.push_code(editor.getValue());
         while (is_running) {
-            let r = machine.sync_run_for(2000n);
+            let r = machine.sync_run_for(20_000n);
             if (!r.can_continue_executing) {
                 set_running_state(false);
             }
+            await new Promise(resolve => setTimeout(resolve, 0));
         }
     } finally {
         machine.free();
