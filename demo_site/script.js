fd6dcaaa1dd54170dbb080e9450d7c8595e1714d
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2026-08-10 17:52:41 +0100

    Adding the play button...

---
+++ demo_site/script.js
@@ -0,0 +1,14 @@
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    var editor = CodeMirror.fromTextArea(document.getElementById('editor'), {
+        lineNumbers: true,
+        mode: '',
+        theme: 'material-darker',
+    });
+});
+
+function run() {
+
+}
+
